f861b132b4146812a71983fa57a6da07747bf9ea
sbb3391 <sbb33391@gmail.com>
2021-05-15 02:54:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+document.getElementsByTagName("main")[0].remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Sam is the champion";
